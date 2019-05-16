1229242c2d08224cfa3c772d12018850ec5a5b65
Cameron Kolkey <Alleyria@gmail.com>
2019-05-16 19:13:49 +0200

    All user stories working

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,28 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to Cocktail.find(params[:cocktail_id])
+    else
+      render 'cocktails/show'
+    end
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
